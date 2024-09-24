9c5ec2e087cc264b22dc52a758405eb9568625a4
RachelLipsker <rjk123770@gmail.com>
2024-09-24 14:54:21 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors")
 
 const corsMiddleware = cors({
-    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:5173"]
+    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:5173", "https://cards2project.onrender.com"]
 })
 
 module.exports = corsMiddleware;
